05e97f86c065706aea49e3a289cbd5a90e3e5374
stgalym <sabitov.taga@gmail.com>
2025-11-27 21:10:40 +0500

    День 6: Готова база, админка, тесты, вход и регистрация

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'), # <--- Добавили это
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from quiz.models import UserTestResult # Импортируем результаты
+from quiz.models import UserTestResult
+from .forms import CustomUserCreationForm
 
-@login_required # Декоратор: не пускает на страницу, если не вошел
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Сразу входим после регистрации
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
 def profile(request):
-    # Достаем результаты ТОЛЬКО текущего пользователя
     results = UserTestResult.objects.filter(user=request.user).order_by('-date_taken')
-
     return render(request, 'profile.html', {'results': results})
